2e049590e1e4bd6677e0ad2c92f0b31b1e420a03
saisumith770 <saisumith812@gmail.com>
2020-12-19 19:22:07 +0400

    added likes based filtering as well to make the algorithm more strict in selection

--- top-content-recommender.py
+++ top-content-recommender.py
@@ -16,21 +16,24 @@ def weighted_rating(array,vote_count,min_vote,rate):
 import pandas as pd 
 
 metaData = pd.DataFrame({
-	"viewer_count" : [10,100,200,300,50,70,160,140,130,100],
-	"likes" : [100000,70005,96545,6223,633,4123,54255,754,55,85]
+	"viewer_count" : [100000,70005,96545,6223,633,4123,54255,754,55,85],
+	"likes" : [1,4,3,2,7,6,8,10,5,10]
 })
 
 mean_viewership_across_dataframe = metaData["viewer_count"].mean()
 minimum_required_viewership = metaData["viewer_count"].quantile(0.75)
+
 #metaData.copy().loc(metaData["viewer_count"] >= minimum_required_voting)
-filtered_items = metaData[metaData["viewer_count"] >= minimum_required_viewership]
+filtered_by_viewer_count = metaData[metaData["viewer_count"] >= minimum_required_viewership]
+minimum_required_likes = filtered_by_viewer_count["likes"].quantile(0.5)
+filter_by_likes = filtered_by_viewer_count[filtered_by_viewer_count["likes"] >= minimum_required_likes]
 
 def weighted_rating(item):
 	viewer_count = item["viewer_count"]
 	likes = item["likes"]
 	return (viewer_count/(viewer_count+minimum_required_viewership) * likes) + (minimum_required_viewership/(minimum_required_viewership+viewer_count) * mean_viewership_across_dataframe)
 
-filtered_items['score'] = filtered_items.apply(weighted_rating,axis=1)
-filtered_items = filtered_items.sort_values('score',ascending=True)
+filter_by_likes['score'] = filter_by_likes.apply(weighted_rating,axis=1)
+filter_by_likes = filter_by_likes.sort_values('score',ascending=True)
 
-print(filtered_items)
+print(filter_by_likes)
